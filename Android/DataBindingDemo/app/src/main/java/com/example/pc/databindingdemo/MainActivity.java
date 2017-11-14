196799026a773090b68b8143f17ca795de965703
tanglie <tanglie23@163.com>
2017-11-14 23:48:08 +0800

    initial commit

--- Android/DataBindingDemo/app/src/main/java/com/example/pc/databindingdemo/MainActivity.java
+++ Android/DataBindingDemo/app/src/main/java/com/example/pc/databindingdemo/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.pc.databindingdemo;
 
-import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,10 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        ActivityBasicBinding binding = DataBindingUtil.setContentView(
-//                this, R.layout.activity_main);
-//        User user = new User("fei", "Liang");
-//        binding.setUser(user);
     }
 }
