084f2f62283184dee02d20e3e2f645530126198f
tanglie <tanglie23@163.com>
2017-11-14 23:54:06 +0800

    实现数据展示

--- Android/DataBindingDemo/app/src/main/java/com/example/pc/databindingdemo/MainActivity.java
+++ Android/DataBindingDemo/app/src/main/java/com/example/pc/databindingdemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pc.databindingdemo;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.pc.databindingdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user = new User("Test", "User");
+        binding.setUser(user);
     }
 }
---
+++ Android/DataBindingDemo/app/src/main/java/com/example/pc/databindingdemo/User.java
@@ -0,0 +1,10 @@
+package com.example.pc.databindingdemo;
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
