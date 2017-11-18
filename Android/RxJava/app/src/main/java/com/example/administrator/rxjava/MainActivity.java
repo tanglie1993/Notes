bc940047a2be9048da334439dd7293f1d7ceafaf
tanglie1993 <tanglie23@163.com>
2017-11-18 15:34:30 +0800

    Hello world

--- Android/RxJava/app/src/main/java/com/example/administrator/rxjava/MainActivity.java
+++ Android/RxJava/app/src/main/java/com/example/administrator/rxjava/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.administrator.rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import rx.Observable;
+import rx.Observer;
+import rx.Subscriber;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getCanonicalName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observer<String> observer = new Observer<String>() {
+
+            @Override
+            public void onCompleted() {
+                Log.i(TAG, "Completed");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "Error");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG, s);
+            }
+        };
+        //使用Observable.create()创建被观察者
+        Observable observable1 = Observable.create(new Observable.OnSubscribe<String>() {
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("Hello");
+                subscriber.onNext("World");
+                subscriber.onCompleted();
+            }
+        });
+        //订阅
+        observable1.subscribe(observer);
     }
 }
