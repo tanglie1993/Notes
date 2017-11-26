4d083f438cb05c21afc6ede5153955795198989d
tanglie <tanglie23@163.com>
2017-11-26 16:08:11 +0800

    click

--- react native/my-app/src/index.js
+++ react native/my-app/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-            {/* TODO */}
-            </button>
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+    </button>
     );
     }
 }
