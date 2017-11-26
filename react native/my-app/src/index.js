5d5366cb1600f2296880506fb778fc3b9ed3b082
tanglie <tanglie23@163.com>
2017-11-26 17:07:48 +0800

    计算获胜者

--- react native/my-app/src/index.js
+++ react native/my-app/src/index.js
@@ -17,13 +17,20 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -36,16 +43,22 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: X';
+            const winner = calculateWinner(this.state.squares);
+            let status;
+            if (winner) {
+                status = 'Winner: ' + winner;
+            } else {
+                status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            }
 
         return (
             <div>
             <div className="status">{status}</div>
             <div className="board-row">
             {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-    </div>
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+        </div>
         <div className="board-row">
             {this.renderSquare(3)}
         {this.renderSquare(4)}
@@ -75,6 +88,26 @@ class Game extends React.Component {
             </div>
     );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
 
 // ========================================
