c51152d50ceb3d6353093eb1ebc65eaf2b8ecf1a
tanglie <tanglie23@163.com>
2017-12-02 20:56:00 +0800

    io initial commit

---
+++ Java/io/src/DirList.java
@@ -0,0 +1,27 @@
+import java.util.regex.*;
+import java.io.*;
+import java.util.*;
+
+public class DirList {
+    public static void main(String[] args) {
+        File path = new File("./files/");
+        String[] list;
+        if(args.length == 0)
+            list = path.list();
+        else
+            list = path.list(new DirFilter(args[0]));
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for(String dirItem : list)
+            System.out.println(dirItem);
+    }
+}
+
+class DirFilter implements FilenameFilter {
+    private Pattern pattern;
+    public DirFilter(String regex) {
+        pattern = Pattern.compile(regex);
+    }
+    public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+    }
+}
