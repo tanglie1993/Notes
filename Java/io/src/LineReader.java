62db0138e2ce3d8f2aabcf585a877a46fdb88cc2
tanglie <tanglie23@163.com>
2017-12-03 00:20:53 +0800

    练习12

--- Java/io/src/LineReader.java
+++ Java/io/src/LineReader.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -20,15 +17,22 @@ public class LineReader {
         try {
             BufferedReader in = new BufferedReader(new FileReader(
                     new File(fileName).getAbsoluteFile()));
+            new File(fileName + " out").createNewFile();
+            PrintWriter writer = new PrintWriter(fileName + " out");
             try {
                 String s;
+                int lineNumber = 0;
                 while((s = in.readLine()) != null) {
                     if(s.contains(searchContent)){
                         texts.add(s.toUpperCase());
                     }
+                    lineNumber++;
+                    writer.print("" + lineNumber + " ");
+                    writer.println(s);
                 }
             } finally {
                 in.close();
+                writer.close();
             }
         } catch(IOException e) {
             e.printStackTrace();
