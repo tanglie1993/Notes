d905b97171a91904231d314581e92bfdcc209d91
tanglie <tanglie23@163.com>
2017-12-03 17:34:10 +0800

    练习20

---
+++ Java/io/src/Directory.java
@@ -0,0 +1,69 @@
+import java.util.regex.*;
+import java.io.*;
+import java.util.*;
+
+public final class Directory {
+    public static File[] local(File dir, final String regex) {
+        return dir.listFiles(new FilenameFilter() {
+            private Pattern pattern = Pattern.compile(regex);
+            public boolean accept(File dir, String name) {
+                return pattern.matcher(
+                        new File(name).getName()).matches();
+            }
+        });
+    }
+    public static File[] local(String path, final String regex) { // Overloaded
+        return local(new File(path), regex);
+    }
+    // A two-tuple for returning a pair of objects:
+    public static class TreeInfo implements Iterable<File> {
+        public List<File> files = new ArrayList<File>();
+        public List<File> dirs = new ArrayList<File>();
+        // The default iterable element is the file list:
+        public Iterator<File> iterator() {
+            return files.iterator();
+        }
+        void addAll(TreeInfo other) {
+            files.addAll(other.files);
+            dirs.addAll(other.dirs);
+        }
+    }
+    public static TreeInfo walk(String start, String regex) { // Begin recursion
+        return recurseDirs(new File(start), regex);
+    }
+    public static TreeInfo walk(File start, String regex) { // Overloaded
+        return recurseDirs(start, regex);
+    }
+    public static TreeInfo walk(File start) { // Everything
+        return recurseDirs(start, ".*");
+    }
+    public static TreeInfo walk(String start) {
+        return recurseDirs(new File(start), ".*");
+    }
+    static TreeInfo recurseDirs(File startDir, String regex){
+        TreeInfo result = new TreeInfo();
+        for(File item : startDir.listFiles()) {
+            if(item.isDirectory()) {
+                result.dirs.add(item);
+                result.addAll(recurseDirs(item, regex));
+            } else // Regular file
+                if(item.getName().matches(regex))
+                    result.files.add(item);
+        }
+        return result;
+    }
+    // Simple validation test:
+    public static void main(String[] args) {
+        TreeInfo treeInfo = walk("D:\\workspace\\Notes\\Java\\io\\out\\production\\io\\");
+        for(File file : treeInfo.files){
+            try {
+                byte[] content = BinaryFile.read(file);
+                for(int i = 0; i < 4; i++){
+                    System.out.println(content[i]);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
