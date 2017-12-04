e39f309c9ef0d6de61dffbdc6239e10893bf17e5
tanglie <tanglie23@163.com>
2017-12-05 00:12:02 +0800

    练习1

--- Java/untitled/src/Main.java
+++ Java/untitled/src/Main.java
@@ -1,6 +1,25 @@
+
 public class Main {
 
+    static class YieldThread extends Thread {
+
+        final int name;
+
+        public YieldThread(int name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 3; i++){
+                System.out.println(""+ name + ": " + i);
+                Thread.yield();
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(int i = 0; i < 10; i++){
+            new YieldThread(i).start();
+        }
     }
 }
