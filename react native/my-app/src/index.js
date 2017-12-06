8c15cb83a3e7e9b6fe19e658568fdde9df6e867f
tanglie <tanglie23@163.com>
2017-12-06 22:35:20 +0800

    练习11

--- react native/my-app/src/index.js
+++ react native/my-app/src/index.js
@@ -77,7 +77,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
-            stepNumber: history.length,
+
             xIsNext: !this.state.xIsNext,
         });
     }
