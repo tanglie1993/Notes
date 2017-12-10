af3bda527ba46f677e9fb0adc47000808f3c8a41
tanglie <tanglie23@163.com>
2017-12-10 18:40:01 +0800

    练习25

---
+++ Java/Concurrent/src/Restaurant.java
@@ -0,0 +1,73 @@
+//: concurrency/Restaurant.java
+// The producer-consumer approach to task cooperation.
+import java.util.concurrent.*;
+
+class Meal {
+    private final int orderNum;
+    public Meal(int orderNum) { this.orderNum = orderNum; }
+    public String toString() { return "Meal " + orderNum; }
+}
+
+class WaitPerson implements Runnable {
+    private Restaurant restaurant;
+    public WaitPerson(Restaurant r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal == null)
+                        wait(); // ... for the chef to produce a meal
+                }
+                System.out.println("Waitperson got " + restaurant.meal);
+                synchronized(restaurant.chef) {
+                    restaurant.meal = null;
+                    restaurant.chef.notifyAll(); // Ready for another
+                }
+            }
+        } catch(InterruptedException e) {
+            System.out.println("WaitPerson interrupted");
+        }
+    }
+}
+
+class Chef implements Runnable {
+    private Restaurant restaurant;
+    private int count = 0;
+    public Chef(Restaurant r) { restaurant = r; }
+    public void run() {
+        try {
+            while(!Thread.interrupted()) {
+                synchronized(this) {
+                    while(restaurant.meal != null)
+                        wait(); // ... for the meal to be taken
+                }
+                if(++count == 10) {
+                    System.out.println("Out of food, closing");
+                    restaurant.exec.shutdownNow();
+                }
+                System.out.println("Order up! ");
+                synchronized(restaurant.waitPerson) {
+                    restaurant.meal = new Meal(count);
+                    restaurant.waitPerson.notifyAll();
+                }
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch(InterruptedException e) {
+            System.out.println("Chef interrupted");
+        }
+    }
+}
+
+public class Restaurant {
+    Meal meal;
+    ExecutorService exec = Executors.newCachedThreadPool();
+    final WaitPerson waitPerson = new WaitPerson(this);
+    final Chef chef = new Chef(this);
+    public Restaurant() {
+        exec.execute(chef);
+        exec.execute(waitPerson);
+    }
+    public static void main(String[] args) {
+        new Restaurant();
+    }
+}
