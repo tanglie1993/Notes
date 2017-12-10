3371f40c5f0451090986b14fcd34a53a0ddeeed8
tanglie <tanglie23@163.com>
2017-12-10 23:26:03 +0800

    练习26

--- Java/Concurrent/src/Restaurant.java
+++ Java/Concurrent/src/Restaurant.java
@@ -1,6 +1,9 @@
 //: concurrency/Restaurant.java
 // The producer-consumer approach to task cooperation.
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class Meal {
     private final int orderNum;
@@ -10,27 +13,31 @@ class Meal {
 
 class WaitPerson implements Runnable {
     private Restaurant restaurant;
-    public WaitPerson(Restaurant r) { restaurant = r; }
+    Condition condition;
+    Lock lock = new ReentrantLock();
+    public WaitPerson(Restaurant r) {
+        restaurant = r;
+        condition = lock.newCondition();
+    }
     public void run() {
         try {
             while(!Thread.interrupted()) {
-                synchronized(this) {
-                    while(restaurant.meal == null)
-                        wait(); // ... for the chef to produce a meal
-                }
+                this.lock.lock();
+                while(restaurant.meal == null)
+                    this.condition.await(); // ... for the chef to produce a meal
+                this.lock.unlock();
                 System.out.println("Waitperson got " + restaurant.meal);
-                synchronized(restaurant.busBoy) {
-                    restaurant.busBoy.notifyAll();
-                }
-                synchronized(this) {
-                    while(!restaurant.isCleaned)
-                        wait(); // ... for the chef to produce a meal
-                }
-                synchronized(restaurant.chef) {
-                    restaurant.meal = null;
-                    restaurant.chef.notifyAll(); // Ready for another
-                }
-
+                restaurant.busBoy.lock.lock();
+                restaurant.busBoy.condition.signalAll();
+                restaurant.busBoy.lock.unlock();
+                this.lock.lock();
+                while(!restaurant.isCleaned)
+                    this.condition.await(); // ... for the chef to produce a meal
+                this.lock.unlock();
+                restaurant.chef.lock.lock();
+                restaurant.meal = null;
+                restaurant.chef.condition.signalAll(); // Ready for another
+                restaurant.chef.lock.unlock();
             }
         } catch(InterruptedException e) {
             System.out.println("WaitPerson interrupted");
@@ -40,20 +47,24 @@ class WaitPerson implements Runnable {
 
 class BusBoy implements Runnable {
     private Restaurant restaurant;
-    public BusBoy(Restaurant r) { restaurant = r; }
+    Condition condition;
+    Lock lock = new ReentrantLock();
+    public BusBoy(Restaurant r) {
+        restaurant = r;
+        condition = lock.newCondition();
+    }
     public void run() {
         try {
             while(!Thread.interrupted()) {
-                synchronized(this) {
-                    while(restaurant.isCleaned)
-//                        System.out.println("BusBoy wait");
-                        wait(); // ... for the chef to produce a meal
-                }
+                this.lock.lock();
+                while(restaurant.isCleaned)
+                    this.condition.await(); // ... for the chef to produce a meal
+                this.lock.unlock();
                 System.out.println("BusBoy clean " + restaurant.meal);
                 restaurant.isCleaned = true;
-                synchronized(restaurant.waitPerson) {
-                    restaurant.waitPerson.notifyAll(); // Ready for another
-                }
+                restaurant.waitPerson.lock.lock();
+                restaurant.waitPerson.condition.signalAll(); // Ready for another
+                restaurant.waitPerson.lock.unlock();
             }
         } catch(InterruptedException e) {
             System.out.println("BusBoy interrupted");
@@ -63,25 +74,31 @@ class BusBoy implements Runnable {
 
 class Chef implements Runnable {
     private Restaurant restaurant;
+    Condition condition;
+    Lock lock = new ReentrantLock();
     private int count = 0;
-    public Chef(Restaurant r) { restaurant = r; }
+    public Chef(Restaurant r) {
+        restaurant = r;
+        condition = lock.newCondition();
+    }
     public void run() {
         try {
             while(!Thread.interrupted()) {
-                synchronized(this) {
-                    while(restaurant.meal != null)
-                        wait(); // ... for the meal to be taken
-                }
+                this.lock.lock();
+                while(restaurant.meal != null)
+                    this.condition.await(); // ... for the meal to be taken
+                this.lock.unlock();
                 if(++count == 10) {
                     System.out.println("Out of food, closing");
                     restaurant.exec.shutdownNow();
+                    return;
                 }
                 System.out.println("Order up! ");
-                synchronized(restaurant.waitPerson) {
-                    restaurant.meal = new Meal(count);
-                    restaurant.isCleaned = false;
-                    restaurant.waitPerson.notifyAll();
-                }
+                restaurant.waitPerson.lock.lock();
+                restaurant.meal = new Meal(count);
+                restaurant.isCleaned = false;
+                restaurant.waitPerson.condition.signalAll();
+                restaurant.waitPerson.lock.unlock();
                 TimeUnit.MILLISECONDS.sleep(100);
             }
         } catch(InterruptedException e) {
@@ -93,10 +110,12 @@ class Chef implements Runnable {
 public class Restaurant {
     Meal meal;
     boolean isCleaned = true;
+    final Lock lock = new ReentrantLock();
     ExecutorService exec = Executors.newCachedThreadPool();
     final WaitPerson waitPerson = new WaitPerson(this);
     final Chef chef = new Chef(this);
     final BusBoy busBoy = new BusBoy(this);
+
     public Restaurant() {
         exec.execute(chef);
         exec.execute(waitPerson);
