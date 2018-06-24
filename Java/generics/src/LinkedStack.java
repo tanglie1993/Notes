fdebcdb1765245fb65bd76d98ff9a9dc5371b85e
tanglie <tanglie23@163.com>
2018-06-24 20:02:04 +0800

    exercise3

---
+++ Java/generics/src/LinkedStack.java
@@ -0,0 +1,34 @@
+/**
+ * Created by pc on 2018/6/24.
+ */
+
+public class LinkedStack<T> {
+    private class Node {
+        T item;
+        Node next;
+        Node() { item = null; next = null; }
+        Node(T item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+        boolean end() { return item == null && next == null; }
+    }
+    private Node top = new Node(); // End sentinel
+    public void push(T item) {
+        top = new Node(item, top);
+    }
+    public T pop() {
+        T result = top.item;
+        if(!top.end())
+            top = top.next;
+        return result;
+    }
+    public static void main(String[] args) {
+        LinkedStack<String> lss = new LinkedStack<String>();
+        for(String s : "Phasers on stun!".split(" "))
+            lss.push(s);
+        String s;
+        while((s = lss.pop()) != null)
+            System.out.println(s);
+    }
+}
