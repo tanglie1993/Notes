b50e916efe44331abf09c659395a6d2b82c95952
tanglie <tanglie23@163.com>
2018-06-30 18:38:37 +0800

    exercise 27

---
+++ Java/generics/src/exercise/Exercise27.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by pc on 2018/6/30.
+ */
+public class Exercise27 {
+
+    public static void main(String[] args) {
+        List<? extends Number> list = new ArrayList<Integer>();
+        list.add(new Integer(1));//wont compile
+
+    }
+}
